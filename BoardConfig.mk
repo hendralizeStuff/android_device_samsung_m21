fb56b8877237021d99e85ef13db43bfbb8889bd6
Hendra Manudinata <hendra32os@gmail.com>
2021-05-17 19:21:48 +0700

    m21: update kernel defconfig name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ TARGET_SCREEN_DENSITY := 420
 
 # Kernel
 BOARD_BOOT_HEADER_NAME       := SRPSL10A001KU
-TARGET_KERNEL_CONFIG := exynos9610-m21nsxx_defconfig
+TARGET_KERNEL_CONFIG := m21_defconfig
 BOARD_DTBO_CFG := $(DEVICE_PATH)/kernel/m21.cfg
 
 # Partitions
